1ca0194a9a142c5c97d90d1193936e3fb248eb45
alephpi <maosicheng98@gmail.com>
2025-08-16 17:56:51 +0800

    wip

--- src/syntex/formula_net/model.py
+++ src/syntex/formula_net/model.py
@@ -2,14 +2,18 @@ import torch
 import torch.nn as nn
 from lightning import LightningModule
 from transformers import MBartConfig, MBartForCausalLM, VisionEncoderDecoderModel
+from transformers.optimization import get_cosine_with_min_lr_schedule_with_warmup
 from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 
 from .encoder import HGNetFormulaConfig, HGNetFormulaHF
 
-tokenizer: PreTrainedTokenizerFast = PreTrainedTokenizerFast.from_pretrained("tokenizer_config.json")
+tokenizer: PreTrainedTokenizerFast = PreTrainedTokenizerFast.from_pretrained("./data/tokenizer")
 
 MODEL_CONFIG = {
-    "encoder_config" : {
+    "tokenizer": {
+        "path": "./data/tokenizer"
+    },
+    "encoder" : {
         "stem_channels": [3, 32, 48],
         "stage_config": {
                 # in_channels, mid_channels, out_channels, num_blocks, downsample, light_block, kernel_size, layer_num
@@ -18,11 +22,11 @@ MODEL_CONFIG = {
                 "stage3": [512, 192, 1024, 3, True, True, 5, 6],
                 "stage4": [1024, 384, 2048, 1, True, True, 5, 6],
             },
-        "output_hidden_dim": 384,
-        "pretrained_backbone": "formulanet_encoder_hgnetv2.pt",
+        "hidden_size": 384,
+        "pretrained_backbone": "./src/syntex/formula_net/formulanet_encoder_hgnetv2.pt",
         "freeeze_backbone": True,
     },
-    "decoder_config": {
+    "decoder": {
         "vocab_size": tokenizer.vocab_size,
         "max_position_embeddings": 1024,
         "d_model": 384,
@@ -30,27 +34,45 @@ MODEL_CONFIG = {
         "decoder_attention_heads": 16,
         "decoder_ffn_dim": 1536,
         "decoder_start_token_id": tokenizer.bos_token_id,
-        "bos_token_id": tokenizer.bos_token.id,
-        "eos_token_id": tokenizer.eos_token.id,
-        "pad_token_id": tokenizer.pad_token.id,
-        "forced_eos_token_id": tokenizer.eos_token.id,
+        "bos_token_id": tokenizer.bos_token_id,
+        "eos_token_id": tokenizer.eos_token_id,
+        "pad_token_id": tokenizer.pad_token_id,
+        "forced_eos_token_id": tokenizer.eos_token_id,
         "layer_norm_eps": 1e-05,
         "is_decoder": True,
     }
 }
 
+TRAINING_CONFIG = {
+    "optimizer": {
+        "lr": 1e-4,
+        "betas": (0.9, 0.999),
+        "weight_decay": 0.05,
+
+
+    },
+    "lr_scheduler":{
+        "min_lr": 1e-8,
+        "num_warmup_steps": 5e3,
+        "num_training_steps": 3e5,
+    }
+}
 
 class FormulaNetLit(LightningModule):
-    def __init__(self, config):
+    def __init__(self, model_config, training_config):
         super().__init__()
-        self.config = config
+        self.model_config = model_config
+        self.training_config = training_config
 
         self.save_hyperparameters()
 
         self.model = VisionEncoderDecoderModel(
-            encoder=HGNetFormulaHF(HGNetFormulaConfig(**MODEL_CONFIG["encoder_config"])),
-            decoder=MBartForCausalLM(MBartConfig(**MODEL_CONFIG["decoder_config"])),
+            encoder=HGNetFormulaHF(HGNetFormulaConfig(**self.model_config["encoder"])),
+            decoder=MBartForCausalLM(MBartConfig(**self.model_config["decoder"])),
         )
+
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), **self.training_config["optimizer"])
+        self.scheduler = get_cosine_with_min_lr_schedule_with_warmup(self.optimizer, **self.training_config["lr_scheduler"])
 
     def forward(self, pixel_values, labels, attention_mask):
         outputs =  self.model.forward(
@@ -77,3 +99,17 @@ class FormulaNetLit(LightningModule):
         loss = self.model_step(batch, batch_idx)
         self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return
+
+    def configure_optimizers(self):
+        return {
+            "optimizer": self.optimizer,
+            "lr_scheduler": {
+                "scheduler": self.scheduler,
+                "interval": "step",
+                "frequency": 1,
+            }
+        }
+
+if __name__ == '__main__':
+    _ = FormulaNetLit(MODEL_CONFIG, TRAINING_CONFIG)
+    print(_)
