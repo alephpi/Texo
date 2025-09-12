720f14782a16a97364c61a0302955bce8c2f4a1d
alephpi <maosicheng98@gmail.com>
2025-09-12 11:27:54 +0200

    better hydra config

--- src/train.py
+++ src/train.py
@@ -2,8 +2,6 @@ import logging
 import hydra
 import lightning as L
 import torch
-from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
-from lightning.pytorch.loggers import TensorBoardLogger
 from omegaconf import DictConfig
 
 from syntex.formula_net.dataset import MERDataModule
@@ -21,38 +19,10 @@ def main(cfg: DictConfig):
     logging.log(logging.INFO, f"Model initialized.")
 
     datamodule = MERDataModule(data_config=cfg.data)
-    logging.log(logging.INFO, f"dataset initialized.")
+    logging.log(logging.INFO, f"Dataset initialized.")
 
-    precision = "bf16-mixed" if torch.cuda.is_bf16_supported() else "16"
-
-    trainer = L.Trainer(
-        default_root_dir=cfg.output_dir,
-        accelerator="auto",
-        devices="auto",
-        precision=precision, # type: ignore
-        # limit_train_batches=2, # for debugging
-        # limit_val_batches=2, # for debugging
-        log_every_n_steps=50,
-        max_epochs=cfg.training.max_epochs,
-        val_check_interval=1000,
-        callbacks=[
-            ModelCheckpoint(
-                filename = "{step}-{val_loss:.4e}-{BLEU:.4f}-{edit_distance:.4f}",
-                save_top_k=10,
-                save_last=True,
-                monitor="val_loss",
-                mode="min",
-                every_n_epochs=1,
-            ),
-            LearningRateMonitor("step"),
-        ],
-        logger=TensorBoardLogger(
-            save_dir=cfg.output_dir,
-            name="",
-            version="",
-            sub_dir="tb_logs"
-            )
-    )
+    trainer = hydra.utils.instantiate(cfg.trainer)
+    logging.log(logging.INFO, f"Trainer initialized.")
 
     if cfg.training.resume_from_ckpt:
         logging.log(logging.INFO, f"Resuming from checkpoint: {cfg.training.resume_from_ckpt}")
