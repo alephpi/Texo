c61cde4610bf6e6a3e3d26d706b7266927dfa03f
alephpi <maosicheng98@gmail.com>
2025-09-29 23:35:14 +0200

    fix

--- scripts/python/run_experiment.py
+++ scripts/python/run_experiment.py
@@ -2,10 +2,15 @@ import subprocess
 
 def run_train_commands():
     base_command = "python src/train.py --multirun --config-dir=./config --config-name=train_slurm.yaml"
-    decoder_layers = [2,4,8,12]
+    decoder_layers = [2]
+    hidden_sizes = [512,768,1024]
     full_commands = []
-    for i, decoder_layer in enumerate(decoder_layers):
-        full_command = base_command + f" model.decoder.decoder_layers={decoder_layer} &"
+    # for i, decoder_layer in enumerate(decoder_layers):
+    #     full_command = base_command + f" model.decoder.decoder_layers={decoder_layer} &"
+    #     full_commands.append(full_command)
+
+    for i, hidden_size in enumerate(hidden_sizes):
+        full_command = base_command + f" model.decoder.tie_word_embeddings=true model.encoder.hidden_size={hidden_size} &"
         full_commands.append(full_command)
 
     for i, full_command in enumerate(full_commands, 1):
--- src/syntex/model/formulanet.py
+++ src/syntex/model/formulanet.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import VisionEncoderDecoderConfig, VisionEncoderDecoderModel
 
-from ..utils.config import OmegaConf
+from ..utils.config import hydra, OmegaConf
 
 
 class FormulaNet(VisionEncoderDecoderModel):
@@ -16,27 +16,19 @@ class FormulaNet(VisionEncoderDecoderModel):
         self.config.pad_token_id = self.decoder.config.pad_token_id
         self.config.eos_token_id = self.decoder.config.eos_token_id
 
-def load_model(config_path="/home/mao/workspace/SynTeX/config/model/FormulaNet-S.yaml"):
-    config = OmegaConf.load(config_path)
-    OmegaConf.resolve(config.model)
-    model = FormulaNet(OmegaConf.to_container(config.model))
-
-    return model
-
-
-if __name__ == '__main__':
-    import pprint
-
-    import torch
+@hydra.main(version_base="1.3.2",config_path="../../../config", config_name="train.yaml")
+def main(cfg):
     import torchinfo
-
-    model = load_model()
+    OmegaConf.set_struct(cfg, False)
+    model = FormulaNet(cfg.model)
+    batch_size = cfg.data.train_batch_size
     dummy_input = {
-        "pixel_values": torch.randn(16, 3, 384, 384, dtype=torch.float32),
-        "decoder_input_ids":torch.randint(0, model.decoder.config.vocab_size, (16, 256), dtype=torch.int32), 
-        "decoder_attention_mask": torch.ones(16, 256, dtype=torch.int32),
+        "pixel_values": torch.randn(batch_size, 3, 384, 384, dtype=torch.float32),
+        "decoder_input_ids":torch.randint(0, model.decoder.config.vocab_size, (batch_size, 256), dtype=torch.int32), 
+        "decoder_attention_mask": torch.ones(batch_size, 256, dtype=torch.int32),
     }
  
     torchinfo.summary(model, input_data=dummy_input, depth=4, col_names=["input_size", "output_size", "num_params", "params_percent"])
-    pprint.pprint(model.config)
- 
+
+if __name__ == '__main__':
+    main()
