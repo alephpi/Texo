871e414e6cf0f880af68096b72b02ec1fba54f93
alephpi <maosicheng98@gmail.com>
2025-10-04 12:32:47 +0200

    build and push to hf hub

--- data/dataset/make_hf_dataset.py
+++ data/dataset/make_hf_dataset.py
@@ -1,5 +1,6 @@
+import argparse
 from pathlib import Path
-from datasets import Dataset, Features, Value
+from datasets import Dataset, Features, Value, DatasetDict
 from tqdm import tqdm
 
 
@@ -18,6 +19,7 @@ def convert_to_hf_dataset(image_dir, text_path, padding_digits=7):
 
         with open(img_path, "rb") as f:
             img_bytes = f.read()
+        assert text, f"Text is empty for {img_path}"
 
         samples.append({
             "image": img_bytes,  # 存 PNG bytes
@@ -34,23 +36,62 @@ def convert_to_hf_dataset(image_dir, text_path, padding_digits=7):
 
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("command", type=str, choices=["build", "test", "push"])
+    args = parser.parse_args()
     parent_dir = Path(__file__).resolve().parent
-    image_dir = parent_dir / "UniMER-1M_merged/images"
-    text_path = parent_dir / "UniMER-1M_merged/train_normalized_.txt"
-    save_dir = parent_dir / "hf_dataset"
-
-    if not save_dir.exists():
-        dataset = convert_to_hf_dataset(image_dir, text_path)
-        print(dataset)
-        dataset.save_to_disk(save_dir )
-        print(f"HuggingFace dataset saved to {save_dir}")
-    else:
+    train_save_dir = parent_dir / "hf_datasets" / "UniMER-Train"
+    test_save_dir = parent_dir / "hf_datasets" / "UniMER-Test"
+    if args.command == "build":
+        if not train_save_dir.exists():
+            train_image_dir = parent_dir / "UniMER-1M_merged/images"
+            train_text_path = parent_dir / "UniMER-1M_merged/train_normalized_.txt"
+            train_dataset = convert_to_hf_dataset(train_image_dir, train_text_path)
+            # --- 顶层 DatasetDict ---
+            print(train_dataset)
+            train_dataset.save_to_disk(train_save_dir )
+            print(f"HuggingFace dataset saved to {train_save_dir}")
+        else:
+            print(f"HuggingFace dataset already exists at {train_save_dir}")
+        if not test_save_dir.exists():
+            test_root = parent_dir / "UniMER-Test"
+            test_splits = ["spe", "cpe", "sce", "hwe"]
+            test_dict = DatasetDict()
+            for split in test_splits:
+                test_image_dir = test_root / split
+                test_text_path = test_root / f"{split}.txt"
+                test_dict[split] = convert_to_hf_dataset(test_image_dir, test_text_path)
+            test_dataset = DatasetDict(test_dict)
+            print(test_dataset)
+            test_dataset.save_to_disk(test_save_dir)
+            print(f"HuggingFace dataset saved to {test_save_dir}")
+        else:
+            print(f"HuggingFace dataset already exists at {test_save_dir}")
+    elif args.command == "test":
         import time
-        dataset = Dataset.load_from_disk(save_dir)
-        print(f"Dataset size: {len(dataset)} samples")
+        train_dataset = Dataset.load_from_disk(train_save_dir)
+        print(f"Dataset size: {len(train_dataset)} samples")
         start_time = time.time()
-        for idx, sample in tqdm(enumerate(dataset), total=len(dataset)):
+        for idx, sample in tqdm(enumerate(train_dataset), total=len(train_dataset)):
             img_bytes = sample["image"]
         total_time = time.time() - start_time
-        print(f"Read {len(dataset)} samples in {total_time:.2f}s")
-        print(f"Speed: {len(dataset)/total_time:.2f} samples/sec")
+        print(f"Read {len(train_dataset)} samples in {total_time:.2f}s")
+        print(f"Speed: {len(train_dataset)/total_time:.2f} samples/sec")
+
+        test_dataset = DatasetDict.load_from_disk(test_save_dir)
+        for name, split in test_dataset.items():
+            start_time = time.time()
+            for idx, sample in tqdm(enumerate(split), total=len(split)):
+                img_bytes = sample["image"]
+            total_time = time.time() - start_time
+            print(f"Read {len(split)} samples in {total_time:.2f}s")
+            print(f"Speed: {len(split)/total_time:.2f} samples/sec")
+    elif args.command == "push":
+        # push to hf hub
+        dataset = Dataset.load_from_disk(train_save_dir)
+        dataset.push_to_hub("alephpi/UniMER-Train")
+        dataset = DatasetDict.load_from_disk(test_save_dir)
+        dataset.push_to_hub("alephpi/UniMER-Test")
+    else:
+        raise ValueError(f"Unknown command {args.command}")
