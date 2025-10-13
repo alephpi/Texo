85cc94149b414b6c4c55389b8008e6752b37d4a1
alephpi <maosicheng98@gmail.com>
2025-10-13 22:51:23 +0200

    feat: trio onnx(with monkey-patch)

--- scripts/python/export_onnx.py
+++ scripts/python/export_onnx.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from optimum.exporters.tasks import TasksManager
-from optimum.exporters.onnx import export
-from optimum.exporters.onnx.model_configs import ViTOnnxConfig
-from transformers import VisionEncoderDecoderModel
+from optimum.exporters.onnx import main_export
+from optimum.exporters.onnx.model_configs import ViTOnnxConfig, VisionEncoderDecoderOnnxConfig
+from transformers import VisionEncoderDecoderModel, VisionEncoderDecoderConfig
 from texo.model.formulanet import FormulaNet
 
 register_tasks_manager_onnx = TasksManager.create_register("onnx")
@@ -17,26 +17,37 @@ class HGNetv2OnnxConfig(ViTOnnxConfig):
 
 def export_onnx():
     path='./model'
-    model = VisionEncoderDecoderModel.from_pretrained(path)
-    onnx_config_constructor = TasksManager.get_exporter_config_constructor(
-        exporter="onnx",
-        model=model,
-        task="image-to-text",
-        library_name="transformers",
-        exporter_config_kwargs={"use_past": True},
-    )
-    onnx_config = onnx_config_constructor(model.config)
-    out = Path("./model/onnx")
+    # model = VisionEncoderDecoderModel.from_pretrained(path)
+    # onnx_config_constructor = TasksManager.get_exporter_config_constructor(
+    #     exporter="onnx",
+    #     model=model,
+    #     task="image-to-text",
+    #     library_name="transformers",
+    # )
+    # onnx_config = onnx_config_constructor(model.config)
+    # out = Path("./model/onnx")
+    # out.mkdir(exist_ok=True)
+
+    # inputs, outputs = export(model, 
+    #                          onnx_config, 
+    #                          out/"model.onnx", 
+    #                          onnx_config.DEFAULT_ONNX_OPSET,
+    #                          input_shapes={"pixel_values": [1, 3, 384, 384]},
+    #                          )
+    # print(inputs)
+    # print(outputs)
+
+    out = Path("./model/trio_onnx")
     out.mkdir(exist_ok=True)
 
-    inputs, outputs = export(model, 
-                             onnx_config, 
-                             out/"model.onnx", 
-                             onnx_config.DEFAULT_ONNX_OPSET,
-                             input_shapes={"pixel_values": [1, 3, 384, 384]},
-                             )
-    print(inputs)
-    print(outputs)
+    # NOTE: this can only export to encoder_model.onnx and decoder_model.onnx,
+    # so I monkey-patched the source code to inject use_past=True to export the decoder_model_with_past.onnx
+    main_export(
+        path,
+        task="image-to-text",
+        output=out,
+        # custom_onnx_configs=custom_onnx_config
+    )
 
 if __name__ == '__main__':
     import debugpy
